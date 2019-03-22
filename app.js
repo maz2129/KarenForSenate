4d52602cece95719f298028a0fe8042481f2c597
Matt Zinman <maz2129@columbia.edu>
2019-03-22 17:29:22 +0000

    add styling to front-end

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+// install express
 var express = require('express'),
-    app     = express();
+    app = express();
     
-app.set('view engine', 'ejs')
-    
+// configure ejs
+app.set("view engine", "ejs");
+
+// configure public directory
+app.use(express.static(__dirname + "/public"));
+
 app.get('/', function(req, res){
    res.render('index'); 
 });
 
+// have server listen
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("SERVER IS RUNNING!!!");
 });
