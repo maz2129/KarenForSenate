30c1e659d2407b9aa2a7f8bbfa786e210a77d7b7
Matt Zinman <maz2129@columbia.edu>
2019-03-22 17:36:00 +0000

    set up post route

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-// install express
+// setup requirements
 var express = require('express'),
-    app = express();
+    app = express(),
+    bodyParser = require('body-parser');
     
 // configure ejs
 app.set("view engine", "ejs");
-
 // configure public directory
 app.use(express.static(__dirname + "/public"));
+// confidure body-parser
+app.use(bodyParser.urlencoded({extended: true}));
 
+// ROUTES
 app.get('/', function(req, res){
    res.render('index'); 
+});
+
+app.post('/', function(req, res){
+   var name = req.body.firstName;
+   res.send(name); 
 });
 
 // have server listen
